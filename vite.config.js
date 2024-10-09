877abaacb6c86132eab7d5fb2c372f360cd465ac
Katalin Czifferi <katica@Katalins-Air.lan>
2024-10-09 14:00:08 +0200

    add vite config

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // root: 'src',
+  base: '/da_recipes/',
+  // server: {
+  //   port: 3000,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:5000',
+  //       changeOrigin: true,
+  //     }
+  //   }
+  // },
+  // build: {
+  //   outDir: '../dist',
+  //   sourcemap: true
+  // },
+  // plugins: [vue()],
+  // resolve: {
+  //   alias: {
+  //     '@': '/src'
+  //   }
+  // },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@import “src/styles/global.scss”;`
+  //     }
+  //   }
+  // }
+})
